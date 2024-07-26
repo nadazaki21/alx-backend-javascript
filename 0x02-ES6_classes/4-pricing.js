a183835428fe4bfccd4a7ede02309716453f8e44
nadazkai21 <nadazaki019@gmail.com>
2024-07-26 21:25:05 +0300

    4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,3 +1,43 @@
 import Currency from "./3-currency";
 
-// export default 
+export default class Pricing extends Currency {
+    constructor(amount, currency) {
+
+        super(currency);
+        this._amount = amount;
+        this._name = currency._name;
+        this._code = currency._code;
+        this._currency = currency;
+    }
+
+    // Getters
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    //Setters
+
+    set amount(newAmount) {
+        this._amount = newAmount;
+
+    }
+
+    set currency(newCurrency) {
+        this._currency = newCurrency;
+    }
+
+    displayFullPrice() {
+        return this._amount + " " + this.name + " " + '(' + this._code + ')'
+
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return (amount * conversionRate);
+    }
+
+
+}
